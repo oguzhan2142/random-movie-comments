9f541da1621e40ee54e53cb5cc136ca1524bc0bb
oguzhan <oguzhan2142@hotmail.com>
2021-11-30 15:49:11 +0300

    add item model

---
+++ app/src/main/java/com/oguzhan/moviecommentsapp/model/Item.kt
@@ -0,0 +1,23 @@
+package com.oguzhan.moviecommentsapp.model
+import com.google.gson.annotations.SerializedName
+
+data class Item(
+    @SerializedName("crew")
+    val crew: String,
+    @SerializedName("fullTitle")
+    val fullTitle: String,
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("imDbRating")
+    val imDbRating: String,
+    @SerializedName("imDbRatingCount")
+    val imDbRatingCount: String,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("rank")
+    val rank: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("year")
+    val year: String
+)
